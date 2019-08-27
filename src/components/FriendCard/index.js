3bfa9f74deca1d8836d382d7bf151befbb05e955
k2k <kadonna@wonderkul.com>
2019-08-26 21:49:46 -0500

    Committing last updates. Still in progress with interactivity

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -3,12 +3,14 @@ import "./style.css";
 
 function FriendCard(props) {
   //set state of card to false by default - "card has not been clicked"
-  const [isClicked, setClick] = useState(false);
+  let [isClicked, setClick] = useState(false);
 
   //each card who's state changes to true, adds 1 to current score
   const [count, setCount] = useState(0);
 
   //a function to increment the count/score and set isClicked
+
+  //if (isClicked === false)
   const increment = () => {
     setCount(count + 1);
     setClick(true);
