bd81a94ed89d8d383d628c576ab4413302b1dc3a
k2k <kadonna@wonderkul.com>
2019-09-05 15:53:29 -0500

    Updates

---
+++ precache-manifest.f275cf025aba8f2de14c3c149db39e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bc6db6fa8328416ea1efa6cdbccb83",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "875cca3ac841fe99ff95",
+    "url": "/clicky-game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "acaf4c4ccd7b6fd3828f",
+    "url": "/clicky-game/static/css/main.07ddc759.chunk.css"
+  },
+  {
+    "revision": "875cca3ac841fe99ff95",
+    "url": "/clicky-game/static/js/2.2d6582d7.chunk.js"
+  },
+  {
+    "revision": "acaf4c4ccd7b6fd3828f",
+    "url": "/clicky-game/static/js/main.184c75d7.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
