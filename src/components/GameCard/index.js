91314655f6caf4171cdf2d9d8608e856d119abd7
k2k <kadonna@wonderkul.com>
2019-09-05 13:21:00 -0500

    fixed shake animation, need a space after qutoe and before new shake class

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -6,9 +6,8 @@ function GameCard(props) {
     <div className="card">
       <div
         onClick={() => props.clicked(props.isClicked, props.id)}
-        className={"img-container" + (props.shake ? "shake" : "")}
+        className={"img-container" + (props.shake ? " shake" : "")}
       >
-        <div></div>
         <img className="img-thumbnail" alt={props.name} src={props.image} />
       </div>
     </div>
